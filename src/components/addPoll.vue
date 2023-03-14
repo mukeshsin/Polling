f3a44398a8b7e746bec0c3e7e140822aea30e857
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-03-14 19:23:01 +0530

    worked on add poll

---
+++ src/components/addPoll.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="formWrapper">
+    <h3 class="poll-head">Add Poll </h3>
+    <p class="poll-para"> Here add a new poll</p>
+    <form @submit.prevent="createPoll">
+        <label>Poll Title:</label>
+        <input type="text" class="pollInput" placeholder="Add a poll title" />
+        <label>Poll Options:</label>
+
+        <div class="inputWrap">
+            <input type="text" class="pollInput" placeholder="add option for poll" />
+            <div class="pollIcon" @click="addOptions">
+                <i class="fa fa-add"></i>
+            </div>
+        </div>
+
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: "addPoll",
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,21 @@
 <template>
-<h1> welcome to polling-system</h1>
+  <div>
+    <h1>Welcome to the polling system</h1>
+    <addPoll></addPoll>
+  </div>
 </template>
 
 <script>
+import addPoll from '../components/addPoll.vue'
+
 export default {
-    name: 'homeView',
-
-    setup() {
-
-    }
+  name: 'HomeView',
+  components: {
+    addPoll,
+  }
 }
 </script>
 
 <style scoped>
-
+/* Add your styles here */
 </style>
