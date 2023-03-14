7e386f090f53085f1fc6f56504a25a711e45275c
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-03-14 15:41:01 +0530

    return operator login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,37 +1,28 @@
 <template>
-<div class="login-box">
+  <div class="login-box">
     <h1 class="head">Login Form</h1>
     <h4 class="headPara">Here login an existing user</h4>
     <form @submit.prevent="handleLogin">
-        <label>Email</label>
-        <input type="email" class="formInput" v-model="signupData.email" />
-        <label>Password</label>
-        <input type="password" class="formInput" v-model="signupData.password" />
-        <span class="errors">{{ loginError }}</span>
-        <button type="submit" class="loginBtn">Login</button>
+      <label>Email</label>
+      <input type="email" class="formInput" v-model="signupData.email" />
+      <label>Password</label>
+      <input type="password" class="formInput" v-model="signupData.password" />
+      <span class="errors">{{ loginError }}</span>
+      <button type="submit" class="loginBtn">Login</button>
     </form>
-</div>
+  </div>
 </template>
 
 <script>
-import {
-    fetchApi
-} from "../composable/loginApi.js";
+import { fetchApi } from "../composable/loginApi.js";
 
 export default {
-    name: "logInPage",
-    setup() {
-        const {
-            signupData,
-            handleLogin,
-            loginError
-        } = fetchApi();
-        return {
-            signupData,
-            handleLogin,
-            loginError,
-        };
-    },
+  name: "logInPage",
+  setup() {
+    return {
+      ...fetchApi(),
+    };
+  },
 };
 </script>
 
